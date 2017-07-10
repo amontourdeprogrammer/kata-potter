43d5af30aa36131920fc0c551922994d75051365
Lisa Durand <lisa@superdudu.net>
2017-07-10 12:17:46 +0200

    add discount for 2 different books

--- lib/price_calculator.rb
+++ lib/price_calculator.rb
@@ -1,9 +1,19 @@
-module PriceCalculator 
+module PriceCalculator
 
 	UNIT_PRICE = 8
 
 	def checkout(books)
-		books.count * UNIT_PRICE
-	end
-	
+    nb_distinct_books = books.uniq.count
+    nb_left_books = books.count - nb_distinct_books
+		nb_distinct_books * UNIT_PRICE * discount(nb_distinct_books) + nb_left_books * UNIT_PRICE
+  end
+
+  def discount(nb_distinct_books)
+    case nb_distinct_books
+    when 2
+      0.95
+    else
+      1
+    end
+  end
 end
--- spec/price_calculator_spec.rb
+++ spec/price_calculator_spec.rb
@@ -4,13 +4,23 @@ include PriceCalculator
 
 describe PriceCalculator do
 
-	it "returns 0 when user buy no books" do 
+	it "returns 0 when user buy no books" do
 		books = []
 		expect(checkout(books)).to eql(0)
 	end
 
-	it "returns 8 when user buy one book" do 
+	it "returns 8 when user buy one book" do
 		book_id = 1
 		expect(checkout([book_id])).to eql(UNIT_PRICE)
 	end
+
+  it "applies a 5% discount when user buy 2 different books" do
+    books = [1, 2]
+    expect(checkout(books)).to eql(2 * UNIT_PRICE * 0.95)
+  end
+
+  it "returns right price when user buys 2 copies of one book and another" do
+    books = [1, 2, 2]
+    expect(checkout(books)).to eql(2 * UNIT_PRICE * 0.95 + UNIT_PRICE)
+  end
 end
